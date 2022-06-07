1edec59d09a23409c0dbc399868ca77ac273899f
Lhotte Louis <louis.lhotte@student-cs.fr>
2022-06-07 10:03:43 +0200

    Changing the plot

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import tweepy
 import matplotlib.pyplot as plt
 import time
+import datetime
 
 # Authentication Keys
 consumerKey = "rP1N2eAUuLP3zmGkTj166zwgs"
@@ -14,7 +15,7 @@ accessTokenSecret = "WShy6U4B1vU5JyooAnxcbLGzfzVhRvOwvzjolOPTywKDG"
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
-
+username = "CeveraRomain"
 
 # Sentiment Analysis
 
@@ -22,10 +23,15 @@ def percentage(part, whole):
      return 100 * float(part) / float(whole)
 
 
-keyword = "Elon Musk"
+keyword = "#Elon Musk"
 noOfTweet = int(input("Please enter how many tweets to analyze: "))
+# start_date = datetime.datetime(2022, 1, 6, 12, 00, 00)
+# end_date = datetime.datetime(2022, 3, 6, 12, 00, 00)
 
-tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(noOfTweet)
+tweets = tweepy.Cursor(api.search_tweets, q=keyword, lang="en").items(noOfTweet)
+
+
+
 
 positive = 0
 negative = 0
@@ -68,7 +74,7 @@ colors = ['yellowgreen', 'gold','red']
 patches, texts = plt.pie(sizes,colors=colors, startangle=90)
 plt.style.use('default')
 plt.legend(patches, labels, loc='best')
-plt.title("Sentiment Analysis Result for keyword=  "+keyword+"" )
+plt.title("Sentiment Analysis Result About "+keyword+"" )
 plt.axis('equal')
 plt.show()
 
