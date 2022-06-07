115cac6724d2af6d693ca77c501ea392b15d0844
Lhotte Louis <louis.lhotte@student-cs.fr>
2022-06-07 15:32:46 +0200

    Creating the file best_algo.py which includes a lot of auxiliary functions that are defined carefully. It has yet to be finished. To be continued...

---
+++ best_algo.py
@@ -0,0 +1,42 @@
+import csv
+import numpy as np
+import pandas as pd
+
+
+Path = ""
+Unknown = []
+Positive = []
+Negative = []
+Neutral = []
+
+with open('eggs.csv', newline='') as csvfile:
+     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     for row in spamreader:
+          Unknown.append(row)
+
+def create_Sim(dataset):
+     """
+     :param dataset: Dataset avec les pseudos des individus labellisés "Pro-ElonMusk" et "Anti-ElonMusk".
+     :return: La matrice de similarité Sim avec les individus labellisés en ligne, les abonnements en colonnes.
+     Chaque indice Sim[i][j] = 1 si l'individu i suit la page j, 0 sinon
+     """
+
+def loop(Sim, Unkwown, Label):
+     """
+     :Input: Matrice de similarité remplie actuellement appelée "Sim", Liste Unknown, Matrice Label des pseudos labellisés "pour",
+     "contre", ou "neutre" à propos d'Elon.
+     :return: Matrice de similarité agrandie (avec les fortements positif, fortements neutre,
+     fortement négatif rentrant dans la matrice)
+     """
+
+     return
+
+
+def main(Unknown, dataset):
+     """
+     :param: dataset (Dataset avec les pseudos des individus labellisés "Pro-ElonMusk" et "Anti-ElonMusk")
+     Unknown (la liste des pseudos non 'classés')
+     :return: Liste des
+     """
+     loop(list)
+     return
